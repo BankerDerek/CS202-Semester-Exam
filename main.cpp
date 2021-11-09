69d28c7a483c6560015d141d9a5c5022b8ff1650
Banker_Derek <89561585+BankerDerek@users.noreply.github.com>
2021-11-08 18:03:11 -0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <windows.h>
+#include <stdio.h>
+#include <ctype.h>
+
+using namespace std;
+
+vector<double > wavFile;
+
+class FileIO
+{
+    public: 
+        FileIO (string filepath)
+        {
+
+        }
+    
+        vector<string> getMetaData ()
+        {
+
+        }
+
+        void importFile ()
+        {
+
+        }
+
+        void exportFile (vector<double> wavFileData, string filePath, vector<string> )
+        {
+
+        }
+
+    private:
+        bool fileExists ()
+        {
+
+        }
+
+
+}; 
+
+class AudioModule
+{
+
+
+};
+
+    // Sultan
+    class Normalization : AudioModule  
+    {
+
+    };
+
+    // Derek
+    class Echo : AudioModule  
+    {
+
+    };
+
+    // Connor
+    class GainAdjustment : AudioModule  
+    {
+
+    };
+
+    // Connor
+    class LowPassFilter : AudioModule  
+    {
+
+    };
+
+    // Sultan
+    class Compression : AudioModule  
+    {
+
+    };
