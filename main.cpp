5e914c706e68bc20c9c40c072a251f04dc800c95
Banker_Derek <derekbanker@nevada.unr.edu>
2021-11-08 17:39:10 -0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <windows.h>
+#include <stdio.h>
+#include <ctype.h>
+
+vector<double> wavFile;
+
+class FileIO
+{
+    public: 
+        FileIO (string filepath)
+        {
+
+        }
+
+
+
+}; 
+
+class AudioModule
+{
+
+};
+
+    class Normalization : AudioModule  
+    {
+
+    };
+
+    class Echo : AudioModule  
+    {
+
+    };
+
+    class GainAdjustment : AudioModule  
+    {
+
+    };
+
+    class LowPassFilter : AudioModule  
+    {
+
+    };
+
+    class Compression : AudioModule  
+    {
+
+    };
