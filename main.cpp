8b6613b1e469a27cb02b29ceb287e0bcd63c3372
Banker_Derek <89561585+BankerDerek@users.noreply.github.com>
2021-12-04 18:13:54 -0800

    User interaction

--- main.cpp
+++ main.cpp
@@ -135,3 +135,61 @@ class FileIO
     {
 
     };
+main(int argc, char *argv[])
+{
+    // Class inintalizations
+    WavFileIO audioFile;
+    
+    
+    // Intoduction
+    cout << endl << "Initializing Audio Editor..." << endl;
+    
+    // Asks the user to open the menu or quit
+    char* userInput;
+    cout << "Type 'e' to exit the program. Type 'm' to go to the menu" << endl;
+    while (true)
+    {
+        cout << "\tInput: ";
+        cin >> userInput;
+
+        if (userInput[0] == 'e')
+        {
+            cout << "\tExiting program..." << endl;
+            return 0;
+        }    
+        if (userInput[0] == 'm')
+        {
+            cout << endl << "Entering Menu..." << endl;
+            break;
+        }
+        cout << "\tInvalid input. Please try again" << endl;
+    }
+
+    // Asks for file and checks if valid
+    cout << "Please provide the path of the file you wish to load." << endl;
+    while (true)
+    {
+        cout << "\tFile path: ";
+        cin >> userInput;
+        // cout << "UserInput: \"" << userInput << "\"" << endl;
+        if (audioFile.validateFile(userInput))
+        {
+            cout << "\tFile loaded successfully." << endl << endl;
+            break;
+        }
+        cout << endl << "\tFile failed to load. Please try a different file." << endl;
+    }
+    
+    // Module selection
+    cout << "Input the number of the module you would like to apply." << endl;
+    cout << "\t1 : Normalization" << endl;
+    cout << "\t2 : Echo" << endl;
+    cout << "\t3 : GainAdjustment" << endl;
+    cout << "\t4 : LowPassFilter" << endl;
+    cout << "\t5 : Compression" << endl;
+
+    
+
+
+    
+}
