4735435becca07948e82777d523faa69472b1df5
Connor Riche <connorriche@nevada.unr.edu>
2021-12-05 17:56:45 -0800

    compression files and header protection for processor file

--- AudioProcessor.h
+++ AudioProcessor.h
@@ -1,5 +1,8 @@
 //Connor Riche
 //AudioProcessor.h
+
+#ifndef AUDIOPROCESSOR_H
+#define AUDIOPROCESSOR_H
 #include "Echo.h"
 #include "GainAdjustment.h"
 #include "normalization.h"
@@ -7,3 +10,5 @@
 #include "LowPassFilter.h"
 
 class Process : public Echo, public GainAdjustment, public Normalization, public Compression, public LowPassFilter{};
+
+#endif
