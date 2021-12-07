22f7188c9e30994704ff14b23c73a0cd30921f13
Connor Riche <connorriche@nevada.unr.edu>
2021-12-06 23:59:48 -0800

    Sultons changes to compression.h

--- Compression.cpp
+++ Compression.cpp
@@ -3,11 +3,18 @@
 #include "Compression.h"
 
 std::vector<float> Compression::compressData(std::vector<float> waveOriginal){
+    /*
     int j = 0;
     for(int i = 0; i < sizeof(waveOriginal); i + compFactor){
         finalData[j] = waveOriginal[i];
         j = j + 1;       
     }
+    return finalData;
+    */
+
+   for(int i = 0; i < waveOriginal.size(); i = i + compFactor){
+		finalData.push_back(waveOriginal[i]);
+	}
     return finalData;
 }
 
