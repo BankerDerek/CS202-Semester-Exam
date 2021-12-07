83eb6919bae0d49a477343eeab9889ec12c9361b
Connor Riche <connorriche@nevada.unr.edu>
2021-12-06 17:01:00 -0800

    added doxygen documentation to AudioProcessor

--- AudioProcessor.h
+++ AudioProcessor.h
@@ -9,6 +9,11 @@
 #include "Compression.h"
 #include "LowPassFilter.h"
 
+///Processor for wave file data
+///
+/** Process class acts as the processor for all the data. The class inherets all the properties of the classes that declare and define the functions for
+ * filtering or modifying the given data. To use, create an object assigned to the "Process" class and then you are free to use any function in any of the
+ * data modifying classes.*/
 class Process : public Echo, public GainAdjustment, public Normalization, public Compression, public LowPassFilter{};
 
 #endif
